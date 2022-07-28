8d61f8f5208e2e08bc733098dcbd3d69c07f017e
Nazarii Dzhevaga <nazariid@fornova.net>
2022-07-28 22:08:46 +0300

    first commit

--- src/test/java/tests/Class2.java
+++ src/test/java/tests/Class2.java
@@ -2,8 +2,6 @@ package tests;
 
 public class Class2 {
     public static void a(){
-        System.out.println("121");
-        System.out.println("First commit");
-        System.out.println("Second commit");
+        System.out.println("first commit");
     };
 }
