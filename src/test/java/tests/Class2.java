7ae005d24abe1f83ea6bb2684f2e073f69cce59f
Nazarii Dzhevaga <nazariid@fornova.net>
2022-07-28 22:15:16 +0300

    third commit

--- src/test/java/tests/Class2.java
+++ src/test/java/tests/Class2.java
@@ -3,5 +3,7 @@ package tests;
 public class Class2 {
     public static void a(){
         System.out.println("first commit");
+        System.out.println("second commit");
+        System.out.println("third commit");
     };
 }
